50b4a6bca50cb364ad2dfff67bc9365db5b10170
NMPGowtham <nmanipremgowtham@gmail.com>
2025-09-13 20:19:57 +0530

    Base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      {/* Routing Here */}
+      {/* Dont change any thing in index.css */}
+      {/* to add components using shadeCN use
+       ex: npx shadcn@latest add button 
+       visit : https://ui.shadcn.com/docs/installation/vite for more details*/}
+    </>
+  )
+}
+
+export default App
